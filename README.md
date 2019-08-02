43426f476b2bd4f17bc18b7fc388b5b43158482b
chuiizeet <chuy.guerra19@outlook.es>
2019-08-02 01:38:40 -0500

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# CImage
+CImage pract
---
+++ cimage/cimage/Controller/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  cimage
+//
+//  Created by imac on 8/2/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Init
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       setupViewComponents()
+    }
+    
+    // MARK: - Helper functions
+    
+    func setupViewComponents() {
+        view.backgroundColor = .red
+    }
+
+
+}
